5f93f023b1a0280c8cbd7ff58ae1d8f58c05dcea
Raphystolage <raphael.morales31@gmail.com>
2023-12-31 18:56:04 +0100

    Change scope of data variable + preparation of answer regex simplest match implementation

--- scripts/solve.js
+++ scripts/solve.js
@@ -1,4 +1,28 @@
 /**
+ * Get quiz data from JavaScript on web page
+ * @return {object} Quiz data in JSON format
+ */
+function fetchQuizData() {
+    var dataScript = document.body.firstChild;
+    while(dataScript.tagName != "SCRIPT") {
+        dataScript = dataScript.nextSibling;
+    }
+
+    var dataString = dataScript.textContent;
+    var dataJSON = JSON.parse(dataString.substring(dataString.indexOf("{"), dataString.lastIndexOf("}") + 1));
+    console.debug("Quiz data : " + dataJSON);
+
+    return dataJSON;
+}
+
+
+/**
+ * @constant Quiz data in JSON format from _page variable
+ */
+const QUIZDATA = fetchQuizData();
+
+
+/**
  * Different types of quiz
  * @enum {string} Key: Name on JetPunk, value: class name of the div on the quiz page
  */
@@ -9,11 +33,12 @@ const QuizTypes = {
     Map: "map-game-page",
     MultipleChoice: "mc-game-page",
     Tile: "tile-game-page"
-}
+};
+
 
 /**
  * Get quiz type (see QuizTypes enum)
- * @returns {string|null} Type name
+ * @return {string|null} Type name
  */
 function getQuizType() {
     var elem = document.body.firstChild;
@@ -31,35 +56,58 @@ function getQuizType() {
     return null;
 }
 
+
 /**
- * Get answers in js code and enter it in the page
+ * Give the simplest string matching with the RegEx passed in parameter
+ * @param {string} regex RegEx used to find simplest match
+ * @return {string} Simplest match
+ */
+function simplestMatch(regex) {
+    return regex;
+}
+
+
+/**
+ * Extract answers from QUIZDATA constant
+ * @param {string} quizType Quiz type value from QuizTypes dictionary
+ * @return {object} List of answers
+ */
+function fetchAnswers(quizType) {
+    var answers = [];
+    for(var answer of QUIZDATA.data.quiz.answers) {
+        if("typeins" in answer) {
+            var typeins = answer.typeins.constructor === Array ? answer.typeins[0] : answer.typeins;
+            answers.push(simplestMatch(typeins.val));
+        } else if("display" in answer) {
+            var display = answer.display;
+            if(display.indexOf("{") >= 0) {
+                display = display.substring(display.indexOf("{") + 1, display.lastIndexOf("}"));
+            }
+            answers.push(display);
+        }
+    }
+
+    return answers;
+}
+
+
+/**
+ * Enter answers on the page
  * @param {string} quizType Quiz type value from QuizTypes dictionary
  * @return {void}
  */
 function solve(quizType) {
-    var solution = document.body.firstChild;
-    while(solution.tagName != "SCRIPT") {
-        solution = solution.nextSibling;
-    }
-
-    var answerString = solution.textContent;
-    var answerJSON = JSON.parse(answerString.substring(answerString.indexOf("{"), answerString.lastIndexOf("}") + 1)).data.quiz.answers;
-    console.debug(answerJSON);
-
-    for(var answer of answerJSON) {
-        answer = answer.display;
-        if(answer.indexOf("{") > 0) {
-            answer = answer.substring(answer.indexOf("{") +1, answer.lastIndexOf("}"));
-        }
-
-        const text_input = document.getElementById("txt-answer-box");
-        text_input.value = answer;
-        text_input.dispatchEvent(new Event('input'));
+    for(var answer of fetchAnswers(quizType)) {
+        console.log(answer);
+        const textInput = document.getElementById("txt-answer-box");
+        textInput.value = answer;
+        textInput.dispatchEvent(new Event('input'));
         console.debug(answer);
     }
 
     console.info("Quizz solved !");
 }
+
 
 /**
  * Place solve button on quiz page
@@ -67,12 +115,18 @@ function solve(quizType) {
  * @return {void}
  */
 function placeSolveButton(quizType) {
-    const solve_button = document.createElement("button");
-    solve_button.textContent = "Solve";
-    solve_button.onclick = () => solve(getQuizType());
-    const text_input = document.getElementById("txt-answer-box");
-    text_input.insertAdjacentElement("afterend", solve_button);
+    var solveButton = document.createElement("button");
+    solveButton.classList.add("blue");
+    solveButton.style.marginLeft = "10px";
+    solveButton.textContent = "Solve";
+    solveButton.onclick = () => solve(quizType);
+    var answerElement;
+    if(quizType == 'Text' | quizType == 'Picture') {
+        test = document.getElementById("txt-answer-box");
+        document.getElementById("txt-answer-box").insertAdjacentElement("afterend", solveButton);
+    }
 }
+
 
 var title = document.getElementsByTagName("h1")[0];
 var quizType = getQuizType();
